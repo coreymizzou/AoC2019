cd394fa27015e030e2026f2c8e5e0b6f87ba0dc5
Corey Hughes <50215735+coreymizzou@users.noreply.github.com>
2019-12-16 09:37:41 -0600

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,40 +1,30 @@
-#open input file
-f = open('input.txt')
-data = f.readline()
+def get_data():
+    with open("day2_input.txt", "r") as f:
+        data = f.read()
+    return [int(x) for x in data.split(",")]
 
-#splits list by comma
-splitData = data.split(",")
 
-#converts items in list to integer type
-for y in range(len(splitData)):
-    splitData[y] = int(splitData[y])
-
-#op add
-def opcode(data, 12, 2):
-    i = 0
+def part1(data, noun=12, verb=2):
+    ptr = 0
     prog = data[:]
     prog[1] = noun
     prog[2] = verb
-    while (data[i] != 99):
-        if (int(data[i]) == 1):
-            a = data[i+1]
-            b = data[i+2]
-            c = data[i+3]
-            data[c] = data[a] + data[b]
-            i += 4
-        elif (int(data[i]) == 2):
-            a = data[i+1]
-            b = data[i+2]
-            c = data[i+3]
-            data[c] = data[a] * data[b]
-            i += 4
-    return data[0]
+    while True:
+        if prog[ptr] == 1:
+            prog[prog[ptr + 3]] = prog[prog[ptr + 1]] + prog[prog[ptr + 2]]
+        elif prog[ptr] == 2:
+            prog[prog[ptr + 3]] = prog[prog[ptr + 1]] * prog[prog[ptr + 2]]
+        elif prog[ptr] == 99:
+            break
+        else:
+            print("SHOULDN'T HAPPEN")
+        ptr += 4
+
+    return prog[0]
+
 
 def part2(data):
     for noun in range(100):
         for verb in range(100):
             if part1(data, noun, verb) == 19690720:
                 return 100 * noun + verb
-
-print(opcode(splitData))
-print(part2(splitData))
